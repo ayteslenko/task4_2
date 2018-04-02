38493ca0c22b738753bdbd9774e081ae2576dfe4
ayteslenko <oleksandr.teslenko@gmail.com>
2018-04-02 23:11:04 +0300

    full links to NTP process was clarified

--- ntp_deploy.sh
+++ ntp_deploy.sh
@@ -11,7 +11,7 @@ sudo sed -i '/^pool /d' /etc/ntp.conf
 sudo sed -i '/more information/a \pool ua.pool.ntp.org iburst' /etc/ntp.conf
 
 sudo cp /etc/ntp.conf /etc/ntp.conf.bak
-sudo service ntp restart
+sudo /usr/sbin/service ntp restart
 
 echo "*/1 * * * * $dir/ntp_verify.sh MAILTO=root@localhost" | crontab -
 
--- ntp_verify.sh
+++ ntp_verify.sh
@@ -1,11 +1,11 @@
-!#/bin/bash
+#!/bin/bash
 
 
 if 	! ps cax | grep -q ntpd
 	
 	then
 	echo -e "\nNOTICE: ntp is not running"
-	sudo systemctl start ntp
+	sudo /usr/sbin/service ntp start
 
 fi
 
@@ -15,7 +15,7 @@ if [ $(diff /etc/ntp.conf.bak /etc/ntp.conf | wc -c)  -ne 0 ]
 	then
 	echo -e "\nNOTICE: /etc/ntp.conf was changed. Calculated diff:\n`diff -U 0 /etc/ntp.conf.bak /etc/ntp.conf`"
 	sudo cp -f /etc/ntp.conf.bak /etc/ntp.conf
-	sudo service ntp restart
+	sudo /usr/sbin/service ntp restart
 
 fi
 
